bda1ffe3f27778924d27d90cff42c8444528c341
Daniel Duque <duque@outlook.com>
2019-11-11 23:02:21 +0100

    Installed express and dependencies

---
+++ server/server.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+
+const indexRouter = require('./routes/index');
+
+// Open database connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    keepAlive: true,
+    reconnectTries: Number.MAX_VALUE
+  })
+  .then(x => {
+    console.log(`Connected to MongoDB! Database name: '${x.connections[0].name}"`);
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB', err);
+  });
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Allow frontend access to API (CORS)
+app.use(cors({
+  credentials: true,
+  origin: ['http://localhost:3000']
+}));
+
+// Routes
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
